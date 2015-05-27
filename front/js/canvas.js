803db9f85f85d5591c56bd1fab1c84798aa819a7
wangqin <wangqin4377@qq.com>
2015-05-28 01:22:50 +0800

    add background

--- front/js/canvas.js
+++ front/js/canvas.js
@@ -1,7 +1,14 @@
 function main(){
 	$(" #mylegend").width( width);
 	$(" #mylegend").css( "margin", "0 auto");
+	loader = new LLoader();
+	loader.addEventListener(LEvent.COMPLETE,loadBitmapdata);
+	loader.load( "images/back.jpg", "bitmapData");
 }
-	
 
+function loadBitmapdata(event){  
+    var bitmapdata = new LBitmapData(loader.content,0,0,width,height);
+    var bitmap = new LBitmap(bitmapdata);
+	addChild(bitmap);
+}
 

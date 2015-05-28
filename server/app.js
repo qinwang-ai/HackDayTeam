ca32f1b6327537d640faf5cb15aa0f957118c13a
GKuChan <gkuchan_root@outlook.com>
2015-05-28 11:48:38 +0800

    code the koa.io server

---
+++ server/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var koa = require('koa.io');
+var middlewares = require('koa-middlewares');
+var routes = require('./routes');
+var config = require('./config');
+var path = require('path');
+
+var app = koa();
+app.keys = config.keys;
+
+//http头中包含处理时间
+app.use(middlewares.rt());
+
+// use cookie session
+app.use(middlewares.cookieSession(app));
+// body parser
+app.use(middlewares.bodyparser());
+
+// 如果游戏已经开始，则禁止进入游戏
+var isPlay = false;
+app.use(function* (next) {
+  if(isPlay) {
+    this.body = '游戏已开始';
+    this.type = 'html';
+  } else {
+    yield* next;
+  }
+});
+
+// static cache
+app.use(middlewares.staticCache({
+  dir: path.join(__dirname, 'static'),
+  buffer: true,
+  gzip: true
+}));
+
+// routes
+app.use(middlewares.router(app));
+routes(app);
+
+app.listen(config.port);
+console.log('The server is listening port %s.', config.port);
+
+app.io.use(function* (next) {
+  console.log('New audience connects.');
+  yield* next;
+  console.log('Some audience disconnects.');
+});
+
+// 游戏开始，所有连接者同时开始
+app.io.route('play', function* (next) {
+  isPlay = true;
+  this.broadcast.emit('play');
+});
+
---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var config = {
+  port: 3000,
+  keys: ['key_for_cookie', 'aposidfjpadsjf']
+};
+
+module.exports = config;

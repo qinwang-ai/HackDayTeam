793e752065be7abb48962cd82f162d67698d8be6
wangqin <wangqin4377@qq.com>
2015-06-02 17:55:05 +0800

    a

---
+++ front/js/websocket.js
@@ -0,0 +1,26 @@
+
+
+var ws=new WebSocket("ws://localhost:8080");
+$(function(){
+    ws.onopen = function(){
+        alert("连接成功");
+    }
+    ws.onerror = function(){
+        alert("连接失败");
+    }
+	ws.onmessage = function(e){
+		alert( e.data);
+	};
+});
+
+//ws.send( $('.textarea').val());
+
+
+if(!window.WebSocket && window.MozWebSocket)
+     window.WebSocket=window.MozWebSocket;
+if(!window.WebSocket){
+    alert("此浏览器不支持WebSocket");
+}
+
+
+

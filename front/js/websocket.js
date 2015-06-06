f27830f69e9c0b66d0689a3de890276d07c14bf3
wangqin <wangqin4377@qq.com>
2015-06-06 12:57:46 +0800

    asd

--- front/js/websocket.js
+++ front/js/websocket.js
@@ -20,20 +20,26 @@ if(!window.WebSocket && window.MozWebSocket)
 if(!window.WebSocket){
     alert("此浏览器不支持WebSocket");
 }
+
+  var socket = io('http://hackday.com/');
+  socket.on('play', function (data) {
+     console.log(data);
+    sockets.emit('result', 'A', 0, true);
+  });
+
+  socket.on('result', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+
+  socket.on('attack', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+
+  socket.on('stop', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+
 */
-
-  var socket = io('http://localhost');
-  socket.on('', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
-
-  socket.on('', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
-  
-  socket.on('', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
